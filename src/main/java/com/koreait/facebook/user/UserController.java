a2e9cc4ae4dd65d8c566edcc75ea303cdc8e392a
heck_evil <heck_evil@naver.com>
2021-06-22 16:55:43 +0900

    2021 06 21 facebook clone

--- src/main/java/com/koreait/facebook/user/UserController.java
+++ src/main/java/com/koreait/facebook/user/UserController.java
@@ -1,15 +1,50 @@
 package com.koreait.facebook.user;
 
-import lombok.Getter;
+import com.koreait.facebook.user.model.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService service;
+
     @GetMapping("/login")
-    public String login (){
-        return "user/login";
+    public void login() {}
+
+    @PostMapping("/join")
+    public String join(UserEntity param){
+        service.join(param);
+        return "redirect:/user/login";
+    }
+    @GetMapping("/join")
+    public String join(){
+        return "user/join";
+    }
+    @PostMapping("/join")
+    public String joinProc(UserEntity userEntity) {
+        service.join(userEntity);
+        return "redirect:/user/login?needEmail=1";
+    }
+
+    @GetMapping("/auth")
+    public String auth(UserEntity param) {
+        int result = service.auth(param);
+        return "redirect:login?auth=" + result;
     }
 }
+
+
+
+
+
+
+
+
+
